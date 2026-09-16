31754adbaff4df17d2771ad23e0af08d5ede51bc
Ryan Buzar <ryanbuzar@icloud.com>
2026-09-16 22:24:30 +0000

    debug date_dimension

---
+++ models/example/date_dimension.sql
@@ -0,0 +1,16 @@
+with CTE as (
+    select
+    TO_TIMESTAMP(STARTED_AT) AS STARTED_AT,
+    DATE(TO_TIMESTAMP(STARTED_AT)) AS DATE_STARTED_AT,
+    HOUR(TO_TIMESTAMP(STARTED_AT)) AS HOUR_STARTED_AT,
+    {{day_type('STARTED_AT')}} AS DAY_TYPE,
+    {{get_season('STARTED_AT')}} AS SEASON_OF_YEAR
+    from
+        {{ source('demo', 'bike_table') }}
+    where 
+        STARTED_AT != 'started_at'
+)
+
+SELECT 
+* 
+FROM CTE;
